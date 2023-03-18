3221227f2057a5d62a526622f92af7386dd21c2e
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-18 16:44:16 +0300

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Passenger.create([{name: "Jeff"}, {name: "Yaitch"}, {name: "Kip"}])
+
+Taxi.create([{number_plate: "KAD", fare: 34},
+{number_plate: "baAD", fare: 4},
+{number_plate: "KDB", fare: 400}])
+
+Ride.create([{passenger_id: 1, taxi_id: 1},
+{passenger_id: 1, taxi_id: 2},
+{passenger_id: 2, taxi_id: 1},
+{passenger_id: 2, taxi_id: 2},
+{passenger_id: 2, taxi_id: 3},
+{passenger_id: 3, taxi_id: 1},
+{passenger_id: 3, taxi_id: 3}  
+])
+
+puts "done seeding"
